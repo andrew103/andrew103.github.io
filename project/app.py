773d03de86fe8bec912ce9dc6a567430f79f6a5d
lmitchell4 <lmitchell4@gmail.com>
2017-11-25 09:45:20 -0800

    Initial commit

---
+++ project/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+    
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
