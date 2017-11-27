e1bd161427b2811f73108cd90264622e6cba38d6
lmitchell4 <lmitchell4@gmail.com>
2017-11-26 20:26:42 -0800

    Update projects api to handle skills and roles arguments

--- project/apis.py
+++ project/apis.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, request
 from project.database.dbconnect import mongo
 
 ############################################################################
@@ -10,8 +10,24 @@ apis = Blueprint("apis", __name__)
 def apiProjects():
   user_table = mongo.db.user
   
-  tmp = user_table.find({"name": "Joe"})[0]
-  del tmp["_id"]    # id object not jsonifiable
+  filter_dict = {}
+  for par_name in request.args.keys():
+    tmp_list = request.args[par_name].split(",")
+    filter_dict[par_name] = { "$in": tmp_list }
+  print filter_dict
   
-  return jsonify(tmp)
   
+  if "skills" in request.args:
+    # Instead of creating a list of skills, could do a regex ...
+    skills_list = request.args["skills"].split(",")
+    cursor = user_table.find(filter_dict)
+    users = list(cursor)
+
+    for i in range(len(users)):
+      users[i]["id"] = str(users[i]["_id"])
+      del users[i]["_id"]   # id object not serializable
+      
+    return jsonify(users = users)
+    
+  # For now, if there's an error, render the index page:
+  return render_template("index.html")
